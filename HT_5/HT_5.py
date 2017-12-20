ec0580ef953f357c9b5060227d2449a15b26f36c
Sashok15 <lordyhard@gmail.com>
2017-12-21 00:42:39 +0300

    fix config

--- HT_5/HT_5.py
+++ HT_5/HT_5.py
@@ -5,16 +5,14 @@ import csv
 import logging
 import re
 
-from config import folder_path, csv_path, logging_path, \
-    url_for_ask, url_for_job, url_for_new, url_for_show, \
-    choices, score, from_date
+from config import *
 
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
+if not os.path.exists(FOLDER_PATH):
+    os.makedirs(FOLDER_PATH)
     logging.info("Create folder 'reports'")
 
 
-logging.basicConfig(filename=logging_path, level=logging.INFO,
+logging.basicConfig(filename=LOGGING_PATH, level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 arg_parser = argparse.ArgumentParser(
@@ -28,7 +26,7 @@ options = arg_parser.parse_args()
 
 logging.info("Program started")
 
-if options.category in choices:
+if options.category in CHOICES:
     print(options)
     logging.info("User selected true-category: "+options.category)
 elif options.category is None:
@@ -42,21 +40,23 @@ data = []
 logging.info("Check category in url")
 try:
     # choices['askstories', 'showstories', 'newstories', 'jobstories']
-    if options.category == choices[0]:
-        response = requests.get(url_for_ask, timeout=5)
+    if options.category == CHOICES[0]:
+        response = requests.get(URL_FOR_ASK, timeout=5)
         data = response.json()
 
-    elif options.category == choices[1]:
-        response = requests.get(url_for_show, timeout=5)
+    elif options.category == CHOICES[1]:
+        response = requests.get(URL_FOR_SHOW, timeout=5)
         data = response.json()
 
-    elif options.category == choices[2]:
-        response = requests.get(url_for_new, timeout=5)
+    elif options.category == CHOICES[2]:
+        response = requests.get(URL_FOR_NEW, timeout=5)
         data = response.json()
 
-    elif options.category == choices[3]:
-        response = requests.get(url_for_job, timeout=5)
+    elif options.category == CHOICES[3]:
+        response = requests.get(URL_FOR_JOB, timeout=5)
         data = response.json()
+    else:
+        raise Exception("Error! not correct the parameters")
 except UnicodeEncodeError:
     logging.error('This is an error message. Unicode encode trouble. '
                   'Check data in request!!!')
@@ -71,7 +71,7 @@ for item in data:
               'v0/item/'+str(item)+'.json?print=pretty'
         response = requests.get(url, timeout=5)
         item_info = response.json()
-        if item_info['score'] >= score and item_info['time'] >= from_date:
+        if item_info['score'] >= SCORE and item_info['time'] >= FROM_DATE:
             info = re.sub('<.+>', '', str(item_info))
             category_info.append(info)
     except UnicodeEncodeError:
@@ -80,7 +80,7 @@ for item in data:
         continue
 logging.info('found all files by filter')
 title = ['number', options.category]
-with open(csv_path, "a", newline='') as f:
+with open(CSV_PATH, "a", newline='') as f:
     writer = csv.writer(f, quoting=csv.QUOTE_ALL)
     writer.writerow(title)
     line_id = 1
--- HT_5/config.py
+++ HT_5/config.py
@@ -1,22 +1,22 @@
 # path
-folder_path = "C:/Users/sashok/GeekHub/GeekHub_python/HT_5/reports"
-logging_path = "reports/hn_parser.log"
-csv_path = "reports/report.csv"
+FOLDER_PATH = "/reports"
+LOGGING_PATH = "reports/hn_parser.log"
+CSV_PATH = "reports/report.csv"
 
 # parameters
-choices = ['askstories', 'showstories', 'newstories', 'jobstories']
-default_parameter = 'newstories'
+CHOICES = ['askstories', 'showstories', 'newstories', 'jobstories']
+DEFAULT_PARAMETER = 'newstories'
 
 # filsters
-from_date = 1510852024
-score = 10
+FROM_DATE = 1510852024
+SCORE = 10
 
 # url for requests
-url_for_new = 'https://hacker-news.firebaseio.com/v0/' \
+URL_FOR_NEW = 'https://hacker-news.firebaseio.com/v0/' \
               'newstories.json?print=pretty'
-url_for_ask = 'https://hacker-news.firebaseio.com/v0/' \
+URL_FOR_ASK = 'https://hacker-news.firebaseio.com/v0/' \
               'askstories.json?print=pretty'
-url_for_show = 'https://hacker-news.firebaseio.com/v0/' \
+URL_FOR_SHOW = 'https://hacker-news.firebaseio.com/v0/' \
                'showstories.json?print=pretty'
-url_for_job = 'https://hacker-news.firebaseio.com/v0/' \
+URL_FOR_JOB = 'https://hacker-news.firebaseio.com/v0/' \
               'jobstories.json?print=pretty'
